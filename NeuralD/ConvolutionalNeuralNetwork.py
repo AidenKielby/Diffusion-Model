ca641c258abf3e9f7f731d5c18a3ad90617ad334
AidenKielby <131564693+AidenKielby@users.noreply.github.com>
2026-01-07 14:36:42 -0500

    added backpropigation
    i hope this works.

--- NeuralD/ConvolutionalNeuralNetwork.py
+++ NeuralD/ConvolutionalNeuralNetwork.py
@@ -7,7 +7,7 @@ import re
 import matplotlib.pyplot as plt
 
 class CNN:
-    # each "layer" specified counts for one conv and one pooling layer (does not inclue input and output layers)
+    # each "layer" specified counts for one conv and one pooling layer (does not inclue input layer)
     # kernel size might need to be odd, dont really care to check weather or not thats true
     def __init__(self, inputSize: tuple[int, int], outputSize: tuple[int, int], kernelSize: int, poolerSize: int, layers: int, kernelsPerLayer: int): 
         self.inputSize = inputSize
@@ -22,6 +22,7 @@ class CNN:
 
         self.kernelLayers = [self.initKernels() for _ in range(layers)]
         self.layers = [] # both pooling and conv layers
+        self.preActivation = [] # post activation function is stored in layers. i think. i am so tired idk anymore
         self.poolerLayers = [self.initPoolers() for _ in range(layers)]
         self.biases = [[random.random() for _ in range(kernelsPerLayer)] for _ in range(layers)]
 
@@ -44,6 +45,15 @@ class CNN:
 
     def leakyReLU_derivative(self, x):
         return 1 if x > 0 else 0.1
+    
+    def leakyReLU_derivative_matrix(self, matrix):
+        out = []
+        for y in matrix:
+            layer = []
+            for x in y:
+                layer.append(self.leakyReLU_derivative(x))
+            out.append(layer)
+        return out
     
     def addInputs(self, inputs: tuple[int, int]):
         self.inputs = inputs
@@ -104,13 +114,16 @@ class CNN:
     def forwardPass(self):
         # input layer -> first hidden layer
         layer = []
+        layerPreAct = []
         self.layers = []
         pad = self.distanceFromKernelCenter
         padded_input = self.pad(self.inputs, pad)
         for kernelIndex in range(len(self.kernelLayers[0])):
             kernelOutputForLayer = []
+            kernelOutputForLayerPreAct = []
             for yIndex in range(pad, len(padded_input) - pad):
                 yLayer = []
+                yLayerForPreAct = []
 
                 yList = padded_input[yIndex]
                 
@@ -124,22 +137,28 @@ class CNN:
 
                     result = self.dotProduct(kernelInput, self.kernelLayers[0][kernelIndex])
                     result += self.biases[0][kernelIndex]
+                    yLayerForPreAct.append(result)
                     result = self.leakyReLU(result)
                     yLayer.append(result)
 
                 kernelOutputForLayer.append(yLayer)
+                kernelOutputForLayerPreAct.append(yLayerForPreAct)
                 
 
             layer.append(kernelOutputForLayer)
+            layerPreAct.append(kernelOutputForLayerPreAct)
         
         self.layers.append(layer)
+        self.preActivation.append(layerPreAct)
         # should now have a layer list that contains the result of each kernel convolving across the input (layer[kernelsResult[y[x]]])
         # now for all middle layers and output
 
         for layerIndex in range(1, self.numLayers+1):
             layer = []
+            layerPreAct = []
             pad = self.distanceFromKernelCenter
-            for kernelIndex in range(len(self.kernelLayers[layerIndex-1])):
+            kernels = len(self.kernelLayers[layerIndex-1]) if layerIndex < self.numLayers else 1
+            for kernelIndex in range(kernels):
                 kernelOutputForLayer = []
                 for kernalOutputOfPrevLayerIndex in range(len(self.layers[layerIndex-1])):
                     padded_input = self.pad(self.layers[layerIndex-1][kernalOutputOfPrevLayerIndex], pad)
@@ -166,11 +185,97 @@ class CNN:
                     
                 avr = self.avrAll(kernelOutputForLayer)
                 avr = self.addValue(avr, self.biases[layerIndex-1][kernelIndex])
+                layerPreAct.append(avr)
                 layer.append(self.applyLeakyReLU(avr))
             self.layers.append(layer)
+            self.preActivation.append(layerPreAct)
 
     def getOutputs(self):
         return self.layers[-1]
+    
+    def compute_prev_gradient(frontGradient, kernel):
+        # flip kernel
+        flipped_kernel = np.flip(np.flip(kernel, 0), 1)
+        
+        pad = kernel.shape[0] - 1
+        padded_gradient = np.pad(frontGradient, ((pad, pad), (pad, pad)), mode='constant')
+        
+        prevGradient = np.zeros((frontGradient.shape[0] + pad, frontGradient.shape[1] + pad))
+        
+        for y in range(prevGradient.shape[0] - kernel.shape[0] + 1):
+            for x in range(prevGradient.shape[1] - kernel.shape[1] + 1):
+                patch = padded_gradient[y:y+kernel.shape[0], x:x+kernel.shape[1]]
+                prevGradient[y, x] += np.sum(patch * flipped_kernel)
+        
+        # crop to match previous layer size
+        crop = (kernel.shape[0] - 1) // 2
+        prevGradient = prevGradient[crop: -crop, crop: -crop]
+        
+        return prevGradient
+    
+    def backpropagate(self, correctOutput: list[list[float]], learningRate: float):
+        # loss of output layer is vector of outpits - vector of expected outputs
+        expectedOutput = np.array(correctOutput)
+        actualOutput = np.array(self.layers[-1])
+
+        outputGradient = (actualOutput - expectedOutput) * np.array(self.leakyReLU_derivative_matrix(self.preActivation[-1][0])) # if the prev value is increased by the ammount in this matrix, the loss will increase
+
+        # output kernel gradients
+        pad = self.distanceFromKernelCenter
+        kernel_grads = []
+        for i in range(len(self.layers[-2])):
+            kernel_grad = np.zeros((self.kernelSize, self.kernelSize))
+            padded_input = self.pad(self.layers[-2][i], pad)
+            for outputIndexY in range(len(self.layers[-1][0])): # this should go thru every output not flattened
+                yOutput = outputGradient[outputIndexY]
+                for outputIndexX in range(len(yOutput)):
+                    kernelOutput = yOutput[outputIndexX]
+
+                    kernelInput = np.array([
+                        [
+                            padded_input[outputIndexY-(yPos-self.distanceFromKernelCenter)][outputIndexX-(xPos-self.distanceFromKernelCenter)] for xPos in range(self.kernelSize)
+                        ] for yPos in range(self.kernelSize)
+                    ])
+
+                    kernel_grad += kernelInput * kernelOutput
+                    kernel_grads.append(kernel_grad)
+         
+        self.kernelLayers[-1][0] = (np.array(self.kernelLayers[-1][0]) - (np.sum(np.array(kernel_grads, axis=0)) * learningRate)).tolist()
+        self.biases[-1][0] -= learningRate * np.sum(outputGradient)
+
+        frontGradient = outputGradient
+        for layerIndex in range(2, len(self.layers)):
+            
+            frontGradSum = 0
+            for kernelIndex in range(len(self.kernelLayers[-layerIndex])):
+                prevGradient = self.compute_prev_gradient(frontGradient, self.kernelLayers[-layerIndex][kernelIndex])
+                frontGradSum += prevGradient
+                pad = self.distanceFromKernelCenter
+                kernel_grads = []
+
+                for i in range(len(self.layers[-layerIndex-1])):
+                    kernel_grad = np.zeros((self.kernelSize, self.kernelSize))
+                    padded_input = self.pad(self.layers[-layerIndex-1][i], pad)
+
+                    for outputIndexY in range(len(self.layers[-layerIndex][kernelIndex])): # this should go thru every output not flattened
+                        yOutput = prevGradient[outputIndexY]
+
+                        for outputIndexX in range(len(yOutput)):
+                            kernelOutput = yOutput[outputIndexX]
+
+                            kernelInput = np.array([
+                                [
+                                    padded_input[outputIndexY-(yPos-self.distanceFromKernelCenter)][outputIndexX-(xPos-self.distanceFromKernelCenter)] for xPos in range(self.kernelSize)
+                                ] for yPos in range(self.kernelSize)
+                            ])
+
+                            kernel_grad += kernelInput * kernelOutput
+                            kernel_grads.append(kernel_grad)
+                
+                self.kernelLayers[-layerIndex][kernelIndex] = (np.array(self.kernelLayers[-layerIndex][kernelIndex]) - (np.sum(np.array(kernel_grads, axis=0)) * learningRate)).tolist()
+                self.biases[-layerIndex][kernelIndex] -= learningRate * np.sum(prevGradient)
+
+            frontGradient = frontGradSum
     
 
 if __name__ == "__main__":
